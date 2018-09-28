c5e5eba03ea0ca2dbcd757b8f07ef58c05147ca2
jixiaomo <2030023229@qq.com>
2018-09-28 20:12:51 +0800

    Update web.php

--- basic2/config/web.php
+++ basic2/config/web.php
@@ -15,7 +15,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '1234',
+            'cookieValidationKey' => '000',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
